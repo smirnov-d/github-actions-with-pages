09cb56796fc51fe52a423799f34e6ea9fbd2fee6
smirnov_d <dim-on-94@mail.ru>
2022-10-27 09:49:48 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/github-actions-with-pages/',
-  plugins: [vue()],
+  plugins: [
+    vue()
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
